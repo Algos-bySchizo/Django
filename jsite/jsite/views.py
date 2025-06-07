9770e5597c2ad4861620eebee1908dc501c0ede0
Usman Junaidy <usmanjunaidy2002@gmail.com>
2025-06-07 15:17:50 +0500

    update

--- jsite/jsite/views.py
+++ jsite/jsite/views.py
@@ -77,10 +77,10 @@ def analyze(request):
     newlineremover = request.POST.get('newlineremover','off')
     spaceremover = request.POST.get('spaceremover','off')
     charcounter = request.POST.get('charcounter','off')
-
+    countedanalyzed= f"character count : {charcounter}" 
     print(removepunc, capitalize, newlineremover, spaceremover, charcounter)
     print(ujtext)
-
+    purposes=[]
     if (removepunc != 'on' and capitalize != 'on' and newlineremover != 'on' and spaceremover != 'on' and charcounter != 'on'):
         return render(request,'error_when_no_util.html')
 
@@ -89,20 +89,26 @@ def analyze(request):
     if removepunc == 'on':
         punctuations = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
         analyzed = ''.join(char for char in analyzed if char not in punctuations)
+        purposes.append("Removed Punctuations")
 
     if capitalize == 'on':
         analyzed = analyzed.upper()
+        purposes.append("Capitalized Text")
 
     if newlineremover == 'on':
         analyzed = ''.join(char for char in analyzed if char != '\n' and char != '\r')
+        purposes.append("Extra New Lines Removed")
 
     if spaceremover == 'on':
         analyzed = ' '.join(analyzed.split())
+        purposes.append("Extra Spaces Removed")
 
-    countedanalyzed = len(analyzed) if charcounter == 'on' else 'char counter off' 
+    if charcounter == 'on': 
+        countedanalyzed=len(analyzed) 
+        purposes.append("Characeters Counted")
 
     return render(request, 'analyze.html', {
-        'purpose': 'Processed',
+        'purpose': ', '.join(purposes),
         'analyzed': analyzed,
         'countedanalyzed': countedanalyzed,
     })
